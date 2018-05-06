bf271c775d8021641aa1f1dba4dc38697e57a57a
bpsourav21 <bpsourav21@gmail.com>
2018-05-07 01:22:07 +0600

    app working fine

--- app/navigator/RootNavigator.js
+++ app/navigator/RootNavigator.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation'
 import Content from '../components/Content';
 import Home from '../components/Home';
@@ -28,13 +30,14 @@ const RootNavigator = StackNavigator({
         },
         headerTintColor: '#fff',
         headerTitleStyle: {
+          textAlign:"center",
           alignSelf: 'center',
           fontWeight: 'bold',
           fontFamily: 'Roboto',
           color: '#fff',
         },
         //  headerLeft: (<View><Text>Back</Text></View>),
-        //  headerRight: (<View><Text>rrr</Text></View>)
+        headerRight: (<View></View>)
       }
     }
   );
