6f91a8dee2fddcb00181db70fd1a52c6870e3792
Mahadi Hasan Sourav <sourav@chaldal.com>
2020-01-23 01:32:05 +0600

    Web view integration from scratch

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { WebView, StyleSheet, View } from 'react-native';
 
 
 class Home extends Component {
@@ -10,15 +10,14 @@ class Home extends Component {
 
         }
     }
-    componentDidMount(props){
+    componentDidMount(props) {
     }
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}> Welcome to React Native!</Text>
-                <Button
-                    title="Go to Content"
-                    onPress={() => this.props.navigation.navigate('Main')}
+                <WebView
+                    source={{uri: 'https://www.facebook.com'}}
+                    style={{marginTop: 20}}
                 />
             </View>
         );
@@ -29,19 +28,22 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        // alignItems: 'center',
+        backgroundColor: '#fafafa',
     },
     welcome: {
-        fontSize: 20,
+        fontFamily: 'Roboto',
+        fontSize: 34,
         textAlign: 'center',
-        margin: 10,
+        color: "#00804A",
+        fontWeight: "bold"
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+    button: {
+        borderRadius: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 20
+    }
 });
 // ======================== REDUX CONNECTORS ========================
 const mapStateToProps = (state) => {
